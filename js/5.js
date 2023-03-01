8e14a2bd2ec1de4422da2db1cd29f02d0439d0cf
Mauricio <mauricioutn2017@gmail.com>
2023-03-01 17:12:22 -0300

    Realizado los ejercicios 4,5,6

---
+++ js/5.js
@@ -0,0 +1,3 @@
+let num= parseInt(prompt(`Ingrese un numero:`));
+let num2= parseInt(prompt(`Ingrese otro numero:`));
+console.log(+(num + num2));
---
+++ js/6.js
@@ -0,0 +1,7 @@
+let num= parseInt(prompt(`Ingrese un numero:`));
+let num2= parseInt(prompt(`Ingrese otro numero:`));
+if(num<num2){
+    document.write(`El mayor es: ${num2}`);
+}else{
+    document.write(`El mayor es: ${num}`)
+}
